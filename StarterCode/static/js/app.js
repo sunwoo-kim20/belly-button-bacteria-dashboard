9418c6ab26515d222943cfff7e4ea6dff5bb1491
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-01 00:09:22 -0700

    added ids to dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,12 @@
+// Use D3 to read data from json file
 d3.json("samples.json").then(function(data) {
-  console.log(data.names);
+  console.log(data.metadata[0]);
+
+  // Add ids to dropdown menu
+  var dropDown = d3.select("#selDataset");
+  data.names.forEach(function(id) {
+    dropDown.append("option")
+      .attr("value", id)
+      .text(id);
+  });
 });
