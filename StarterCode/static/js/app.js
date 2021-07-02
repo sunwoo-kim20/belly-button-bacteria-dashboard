75ed3a7ed6736d52ad49e8954aa1506e7fa6d4b7
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-02 10:51:50 -0700

    added event listeners and form tag

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,4 +9,27 @@ d3.json("samples.json").then(function(data) {
       .attr("value", id)
       .text(id);
   });
+
+  // Create array of dictionaries to hold data
+
+  // Set up event for dropdown and form
+  var dropdown = d3.select("#selDataset");
+  var form = d3.select("#form");
+
+  dropdown.on("change", genCharts);
+  form.on("submit", genCharts);
+
+  function genCharts() {
+    // Prevent page refresh
+    d3.event.preventDefault();
+
+    // Horizontal Bar Chart
+
+    // Bubble Chart
+
+    // Demographic info table
+  }
+
+
+
 });
